998c666d5da076960824a5873aa4c6ae651f96df
VinhNguyenUte <valtriek99@gmail.com>
2025-02-21 08:51:49 +0700

    lan 2

--- BT91_CCB/BT91_CCB/Program.cs
+++ BT91_CCB/BT91_CCB/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text;
+
+namespace BT91_CCB
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Các số nguyên dương lẻ nhỏ hơn 100:");
+
+            for (int i = 1; i < 100; i += 2)
+            {
+                Console.Write(i + " ");
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
