1f78fe5f5d1e640dbe930fc76c00774755e59fa0
VinhNguyenUte <valtriek99@gmail.com>
2025-02-21 08:47:14 +0700

    lan 2

--- BT41_CCB/BT41_CCB/Program.cs
+++ BT41_CCB/BT41_CCB/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text;
+
+namespace BT41_CCB
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.Write("Nhập số nguyên dương n: ");
+            int n = int.Parse(Console.ReadLine());
+
+            double S = 1.0; // Giá trị ban đầu
+            for (int i = n - 1; i >= 0; i--)
+            {
+                S = 1.0 / (1 + S); // Tính từ trong ra ngoài
+            }
+
+            Console.WriteLine($"Giá trị của S({n}) = {S}");
+            Console.ReadKey();
+        }
+
+    }
+}
