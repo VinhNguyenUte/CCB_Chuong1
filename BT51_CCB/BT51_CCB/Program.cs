044f6be78f105f03b24cace6461e09daad0747a0
VinhNguyenUte <valtriek99@gmail.com>
2025-02-21 08:47:59 +0700

    lan 2

--- BT51_CCB/BT51_CCB/Program.cs
+++ BT51_CCB/BT51_CCB/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text;
+
+namespace BT51_CCB
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.Write("Nhập số nguyên dương n: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int maxDigit = 0; // Biến lưu chữ số lớn nhất
+
+            while (n > 0)
+            {
+                int digit = n % 10; // Lấy chữ số cuối
+                if (digit > maxDigit)
+                {
+                    maxDigit = digit; // Cập nhật chữ số lớn nhất
+                }
+                n /= 10; // Bỏ chữ số cuối
+            }
+
+            Console.WriteLine($"Chữ số lớn nhất là: {maxDigit}");
+            Console.ReadKey();
+        }
+
+    }
+}
