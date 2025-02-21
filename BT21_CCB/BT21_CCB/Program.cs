ec3419b7b0c832e27f3ad6adcb0315e43dfcb740
VinhNguyenUte <valtriek99@gmail.com>
2025-02-21 08:45:38 +0700

    lan 2

--- BT21_CCB/BT21_CCB/Program.cs
+++ BT21_CCB/BT21_CCB/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text;
+
+namespace BT21_CCB
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.Write("Nhập số nguyên dương n: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+            for (int i = 1; i * i <= n; i++) // Duyệt từ 1 đến √n
+            {
+                if (n % i == 0) // Nếu i là ước của n
+                {
+                    sum += i; // Cộng ước số nhỏ hơn
+                    if (i != n / i) // Nếu ước số lớn hơn khác i (tránh trùng khi i * i = n)
+                    {
+                        sum += n / i;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Tổng các ước số của {n} là: {sum}");
+            Console.ReadKey();
+        }
+
+    }
+}
