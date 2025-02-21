f3f650ad462d7d66b28efc251b31d8bf4c66cdd0
VinhNguyenUte <valtriek99@gmail.com>
2025-02-21 18:23:50 +0700

    hoan thanh bai 5 lan 3

--- BT41_CCB/BT41_CCB/Program.cs
+++ BT41_CCB/BT41_CCB/Program.cs
@@ -9,15 +9,15 @@ namespace BT41_CCB
         {
             Console.OutputEncoding = Encoding.UTF8;
             Console.Write("Nhập số nguyên dương n: ");
-            int n = int.Parse(Console.ReadLine());
+            int bien223 = int.Parse(Console.ReadLine());
 
             double S = 1.0; // Giá trị ban đầu
-            for (int i = n - 1; i >= 0; i--)
+            for (int i = bien223 - 1; i >= 0; i--)
             {
                 S = 1.0 / (1 + S); // Tính từ trong ra ngoài
             }
 
-            Console.WriteLine($"Giá trị của S({n}) = {S}");
+            Console.WriteLine($"Giá trị của S({bien223}) = {S}");
             Console.ReadKey();
         }
 
