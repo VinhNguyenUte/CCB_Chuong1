37aa8df8d6dd4846c65e7b3de20f9e193b8a3eec
VinhNguyenUte <valtriek99@gmail.com>
2025-02-21 18:24:32 +0700

    hoan thanh bai 6 lan 3

--- BT51_CCB/BT51_CCB/Program.cs
+++ BT51_CCB/BT51_CCB/Program.cs
@@ -9,18 +9,18 @@ namespace BT51_CCB
         {
             Console.OutputEncoding = Encoding.UTF8;
             Console.Write("Nhập số nguyên dương n: ");
-            int n = int.Parse(Console.ReadLine());
+            int bien223 = int.Parse(Console.ReadLine());
 
             int maxDigit = 0; // Biến lưu chữ số lớn nhất
 
-            while (n > 0)
+            while (bien223 > 0)
             {
-                int digit = n % 10; // Lấy chữ số cuối
+                int digit = bien223 % 10; // Lấy chữ số cuối
                 if (digit > maxDigit)
                 {
                     maxDigit = digit; // Cập nhật chữ số lớn nhất
                 }
-                n /= 10; // Bỏ chữ số cuối
+                bien223 /= 10; // Bỏ chữ số cuối
             }
 
             Console.WriteLine($"Chữ số lớn nhất là: {maxDigit}");
